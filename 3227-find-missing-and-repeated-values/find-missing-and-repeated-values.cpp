d4d86a5aa5a37ca993ff2cd63fe11c5765c9f2b1
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-03-22 23:49:15 +0530

    Time: 5 ms (59.35%) | Memory: 27 MB (57.89%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+
+
+
+        int n = grid.size();
+        int duplicate = -1;
+        int missing = -1;
+
+        vector<int> nums;
+
+        for (int i =0; i <n; i++){
+            for (int j=0; j <n; j++){
+                nums.push_back(grid[i][j]);
+            }
+        }
+
+        for (int i =0; i <nums.size(); i++){
+            int idx = abs(nums[i])-1;
+            
+            if(nums[idx]<0){
+                duplicate = abs(nums[i]);
+            }
+            else{
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        for (int i = 0; i <nums.size(); i++){
+            if (nums[i]>0) {
+                missing = i+1;
+                break;
+            }
+        }
+        return {duplicate,missing};
+    }
+};
