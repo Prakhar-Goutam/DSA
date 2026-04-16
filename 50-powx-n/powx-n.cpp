2160abfcd9ac15a6ea506859227b16e932b2c325
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-04-16 20:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.75%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,25 +3,31 @@ public:
     double myPow(double x, int n) {
 
         long long m = n;
+
         if (m<0) {
-            x = 1/x;
             m = -m;
+            x= 1/x;
         }
 
         double ans = 1;
 
         while (m>0) {
-            if (m%2==1) {
-                ans= ans*x;
+
+            if (m%2 == 1) {
+
+                ans*= x;
                 m--;
             }
-            else{
-                m=m/2;
-                x=x*x;
+
+            else {
+
+                m = m/2;
+                x = x*x;
             }
         }
 
         return ans;
+
         
     }
 };
