b537d55cf046e5e5d5996a81457da0ae6982ed97
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-04-17 15:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (13.41%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
 
-        vector<char> ans;
-        int count = -1;
+        string ans = "";
+        int count = 0;
 
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i<s.size(); i++) {
 
-            if (s[i]== '(') {
+            if(s[i]=='(') {
 
-                if(count!=-1) {
-                    ans.push_back(s[i]);   
+                if(count!=0) {
+
+                    ans.push_back(s[i]);
+                    count++;
                 }
                 count++;
             }
 
             else {
 
-                if(count!=0) {
-                    ans.push_back(s[i]);
-                }
+                if (count != 1){
 
+                    ans.push_back(s[i]);
+                    count--;
+                }
                 count--;
             }
         }
