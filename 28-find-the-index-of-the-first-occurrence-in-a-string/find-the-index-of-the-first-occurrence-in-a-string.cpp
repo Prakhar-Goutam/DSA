ddd066ec9f9295c21bcc74e7c624e21519f3be76
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-04-17 23:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (73.66%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,21 +3,19 @@ public:
     int strStr(string s, string t) {
 
         int m = s.size();
-        int n =  t.size();
+        int n = t.size();
 
-        for (int i =0; i <m; i++) {
+        if (n>m) return -1;
 
-            string ans = "";
+        for (int i = 0; i<= m-n; i++) {
 
-            for (int j = i; j<m; j++) {
+            int j = 0;
 
-                ans+=s[j];
-
-                if(ans==t) {
-                    return i;
-                    break;
-                }
+            while (j<n && s[i+j] == t[j]) {
+                j++;
             }
+
+            if (j==n) return i;   
         }
 
         return -1;
