1220568578d63852cdedf9c1914c017ac1a076ed
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-04-23 01:38:47 +0530

    Time: 107 ms (66.99%) | Memory: 8.9 MB (85.83%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -7,22 +7,18 @@ public:
 
         for (int i =0 ; i<n ;i++) {
 
-            string s1 = "";
+            int count[26] = {0};
 
             for (int j =i ; j< n; j++) {
 
-                s1+=s[j];
-                int count[26] = {0};
-
-                for(int z = 0; z<s1.size(); z++) {
-                    count[s1[z] - 'a']++;
-                }
+                
+                count[s[j]-'a']++;
 
                 int Maxl = INT_MIN;
                 int Minl = INT_MAX;
 
                 for (int y = 0; y<26; y++) {
-                    if (count[y] != 0) {
+                    if (count[y] >0) {
                         Maxl = max(Maxl,count[y]);
                         Minl = min(Minl,count[y]);
                     }
