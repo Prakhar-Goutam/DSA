ef4014f29ebf150fb624c5627900dd10ad53bfba
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-05-01 00:22:43 +0530

    Time: 72 ms (69.48%) | Memory: 98.8 MB (30.08%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
 
-        int n = nums.size();
+        unordered_map<int,int> mpp;
 
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i<nums.size() ; i++) {
 
-            for (int j = i+1; j<= i+k && j<n; j++) {
+            if(mpp.find(nums[i]) != mpp.end()) { ///// mtlab map mai exist krta hai
 
-                if (nums[i]==nums[j]){
+                if(abs(i-mpp[nums[i]]) <= k) {
                     return true;
                 }
+                else{
+                    mpp[nums[i]] = i;
+                }
             }
+
+            mpp[nums[i]] = i;
         }
+
         return false;
+        
     }
 };
