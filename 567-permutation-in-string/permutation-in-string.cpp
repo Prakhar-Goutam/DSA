a5f5e46be9fe422a54dbdc1a24f6d1b5e29f832a
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-05-04 04:19:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (98.54%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -5,32 +5,34 @@ public:
         int m = s1.size();
         int n = s2.size();
 
-        if (m>n)return false;
+        if (m>n) return false;
 
-        vector<int>freq1 (26,0);
-        vector<int>freq2 (26,0);
+        vector<int> freq(26,0);
 
-        for (int i =0; i<m; i++) {
-            freq1[s1[i]-'a']++;  
+        for (char ch : s1) {
+            freq[ch - 'a']++;
         }
 
-        int r = 0;
         int l = 0;
+        int count = m;
 
-        while(r<n) {
+        for (int r =0; r<n; r++) {
 
-            freq2[s2[r]-'a']++;
+            if (freq[s2[r]-'a'] >0) {
+                count--;
+            }
 
-            while(r-l+1>m) {
-                freq2[s2[l]-'a']--;
+            freq[s2[r]-'a']--;
+
+            if (r-l+1 >m) {
+                if (freq[s2[l]-'a'] >= 0) {
+                    count++;
+                }
+                freq[s2[l]-'a']++;
                 l++;
             }
 
-            if (freq1 == freq2) {
-                return true;
-            }
-
-            r++;
+            if (count==0)return true;
         }
 
         return false;
