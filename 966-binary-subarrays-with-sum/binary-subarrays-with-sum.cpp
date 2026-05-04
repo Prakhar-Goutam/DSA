892a1de89bfd7941b6a2442416ccb7d0bac43507
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-05-04 22:21:33 +0530

    Time: 20 ms (25.12%) | Memory: 38.6 MB (26.58%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-    int func (vector<int> & nums, int goal) {
-        
+        unordered_map<int , int> mpp;
+        mpp[0] = 1;
         int n = nums.size();
-        int l = 0;
-        int r = 0;
         int sum = 0;
         int count = 0;
 
-        while (r<n) {
+        for (int i = 0; i<n; i++) {
 
-            sum += nums[r];
+            sum += nums[i];
+            int prev = sum - goal;
 
-            while (sum > goal && l<=r) {    ///// (l<=r) likhne ki jarurat nahi hai. 
-                sum -= nums[l];
-                l++;
+            if (mpp.find (prev) != mpp.end()) {
+                count += mpp[prev];
             }
 
-              // if (sum<=goal) {  ///// if not needed anymore kyoki while wali condition window shrink kr krke, sum ko less than equal to k le hi aayega.
-                count += r-l+1;
-           ///// }
-
-            r++;
+            mpp[sum]++;
 
         }
 
         return count;
-    }
-
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-
-        if (goal < 0) return 0;
-        return func(nums,goal) - func (nums, goal-1);
         
     }
 };
