ce079fe1dd580cd0f6efc3faa4a53dd639483265
Prakhar Goutam <prakhargoutam1@gmail.com>
2026-06-25 23:53:48 +0530

    Time: 7 ms (69.02%) | Memory: 12.5 MB (77.41%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,69 +1,40 @@
 class Solution {
 public:
-
-    int countT (string &s , int k) {
-
-        int n = s.size();
-        int l = 0;
-        int count = 0;
-        int maxL = 0;
-
-        for (int r = 0; r< n; r++) {
-
-            if (s[r]=='F') {
-                count++;
-            }
-
-            while (count>k) {
-                if (s[l]=='F') {
-                    count--;
-                }
-                l++;
-            }
-
-            maxL = max(maxL , r-l+1);
-        }
-
-        return maxL;
-    }
-
-    int countF (string &s, int k) {
-
-        int n = s.size();
-        int l = 0;
-        int count = 0;
-        int maxL = 0;
-
-        for (int r = 0; r< n; r++) {
-
-            if (s[r]=='T') {
-                count++;
-            }
-
-            while (count>k) {
-                if (s[l]=='T') {
-                    count--;
-                }
-                l++;
-            }
-
-            maxL = max(maxL , r-l+1);
-        }
-
-        return maxL;
-
-    }
-
-
     int maxConsecutiveAnswers(string s, int k) {
 
-        int T = countT(s,k);
-        int F = countF(s,k);
+        int n = s.size();
+        int tCount = 0;
+        int fCount = 0;
+        int maxF = 0;
+        int maxL = 0;
+        int l = 0;
+        int r = 0;
 
-        if (T>F) return T;
-        else{
-            return F;
+        while (r<n) {
+
+            if (s[r]=='T') {
+                tCount++;
+            }
+
+            if (s[r]=='F') {
+                fCount++;
+            }
+
+            maxF = max(maxF , max(tCount , fCount));
+
+            while (r-l+1 - maxF > k) {
+
+                if (s[l]=='T') tCount--;
+                else {
+                    fCount--;
+                }
+                l++;
+            }
+
+            maxL = max (maxL , r-l+1);
+            r++;
         }
 
+        return maxL; 
     }
 };
